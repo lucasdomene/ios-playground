bea21b18217fc4339f036e28d927dc2ca7e56f5a
Lucas Domene <lucas.firmo@gmail.com>
2025-04-29 18:06:02 -0300

    feat: WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Lucas Firmo on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
