0a8beb4f40e951f4f82a1a299ff7729271f7c8a9
Lucas Domene <lucas.firmo@gmail.com>
2025-05-02 19:35:25 -0300

    feat: WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Lucas Firmo on 02/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
